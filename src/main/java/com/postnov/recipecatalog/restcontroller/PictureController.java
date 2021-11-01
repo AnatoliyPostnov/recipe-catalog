6317fdf1378b49f64ddb65334fe46acbfe7c0c0c
postnov <a.postnov@cft.ru>
2021-11-01 16:59:41 +0300

    Добавил PictureStore

--- src/main/java/com/postnov/recipecatalog/restcontroller/PictureController.java
+++ src/main/java/com/postnov/recipecatalog/restcontroller/PictureController.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @RestController
 @RequiredArgsConstructor
 public class PictureController {
